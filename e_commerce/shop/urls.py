c3d7efeafeb068d794dc0e4e0395451976610472
Zamiel01 <nkuinmbeng@gmail.com>
2024-12-20 06:51:35 -0800

    templates

--- e_commerce/shop/urls.py
+++ e_commerce/shop/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import UserViewSet, ProductViewSet, OrderViewSet, ReviewViewSet
-
+from .views import RegisterView, LoginView, home
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
 router.register(r'products', ProductViewSet)
@@ -9,5 +9,8 @@ router.register(r'orders', OrderViewSet)
 router.register(r'reviews', ReviewViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('', home, name='home'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
 ]
+
--- e_commerce/shop/views.py
+++ e_commerce/shop/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect 
+from django.views import View
 # Create your views here.
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from rest_framework import viewsets
 from .models import User, Product, Order, Review
 from .serializers import UserSerializer, ProductSerializer, OrderSerializer, ReviewSerializer
@@ -20,3 +22,33 @@ class OrderViewSet(viewsets.ModelViewSet):
 class ReviewViewSet(viewsets.ModelViewSet):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+
+
+class RegisterView(View):
+    def get(self, request):
+        form = UserCreationForm()
+        return render(request, 'shop/register.html', {'form': form})
+
+    def post(self, request):
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+        return render(request, 'shop/register.html', {'form': form})
+
+class LoginView(View):
+    def get(self, request):
+        form = AuthenticationForm()
+        return render(request, 'shop/login.html', {'form': form})
+
+    def post(self, request):
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+        return render(request, 'shop/login.html', {'form': form})
+
+def home(request):
+    return render(request, 'shop/home.html')
